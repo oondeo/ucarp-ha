fb2b5dd16102bcf54f7c1791b2ac4e36e926db9a
Tit Petric <tit.petric@monotek.net>
2016-12-28 21:19:05 +0100

    add ipv6 support, netmask support

--- ucarp/down.sh
+++ ucarp/down.sh
@@ -1,17 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 DEV=$1
 VIP=$2
 IPDEV=$3
-shift;
+shift 3
 
-if [ -z "$VIP" ]
-then
+if [ ! -z "$1" ]; then
+	IPS="$@"
+fi
+
+if [ -z "$VIP" ]; then
 	echo "$0 interface vip [additional ips...]"
 fi
 
-for i in $@
+for IP in $VIP $IPS
 do
-	echo "- del $i on $IPDEV"
-	ip addr del $i/32 dev $IPDEV
+	# if > 0, we have netmask also
+	NETMASK="${IP//[^\/]}"
+	# if > 0, we have ipv6 notation
+	IPV6="${IP//[^\:]}"
+
+	echo "> Removing $IP on $IPDEV"
+
+	if (( ${#IPV6} > 0 )); then
+		# adding ipv6 addr here
+		BITS=64
+		if (( ${#NETMASK} > 0 )); then
+			ip -6 addr del $IP dev $IPDEV
+		else
+			ip -6 addr del $IP/$BITS dev $IPDEV
+		fi
+	else
+		# adding ipv6 addr here
+		BITS=32
+		if (( ${#NETMASK} > 0 )); then
+			ip addr del $IP dev $IPDEV
+		else
+			ip addr del $IP/$BITS dev $IPDEV
+		fi
+	fi
 done
+
+echo "> Bringing down interface link $IPDEV"
 ip link set $IPDEV down
--- ucarp/up.sh
+++ ucarp/up.sh
@@ -1,17 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 DEV=$1
 VIP=$2
 IPDEV=$3
-shift
+shift 3
 
-if [ -z "$VIP" ]
-then
+if [ ! -z "$1" ]; then
+	IPS="$@"
+fi
+
+if [ -z "$VIP" ]; then
 	echo "$0 interface vip [additional ips...]"
 fi
 
-for i in $VIP $IPS
+for IP in $VIP $IPS
 do
-	echo "- add $i on $IPDEV"
-	ip addr add $i dev $IPDEV
+	# if > 0, we have netmask also
+	NETMASK="${IP//[^\/]}"
+	# if > 0, we have ipv6 notation
+	IPV6="${IP//[^\:]}"
+
+	echo "> Adding $IP on $IPDEV"
+
+	if (( ${#IPV6} > 0 )); then
+		# adding ipv6 addr here
+		BITS=64
+		if (( ${#NETMASK} > 0 )); then
+			ip -6 addr add $IP dev $IPDEV
+		else
+			ip -6 addr add $IP/$BITS dev $IPDEV
+		fi
+	else
+		# adding ipv6 addr here
+		BITS=32
+		if (( ${#NETMASK} > 0 )); then
+			ip addr add $IP dev $IPDEV
+		else
+			ip addr add $IP/$BITS dev $IPDEV
+		fi
+	fi
 done
+
+echo "> Bringing up interface link $IPDEV"
 ip link set $IPDEV up
