e153391bcc25a74e70b724b32bf28605031787e6
Tit Petric <tit.petric@monotek.net>
2016-12-28 22:17:16 +0100

    more verbosity

--- ucarp/ucarp.sh
+++ ucarp/ucarp.sh
@@ -7,4 +7,6 @@ echo "Using id: $VHID"
 NET=`ip addr show dev $DEV|awk '/inet[^6]/ {print $2}'`
 RIP=`echo $NET|cut -d/ -f1`
 
+echo "REAL IP of UCARP instance: $RIP"
+
 exec ucarp -i "$DEV" -s "$RIP" -v "$VHID" -p "$PASS" -a "$VIP" -x "$IPS" -u /ucarp/up.sh -d /ucarp/down.sh -x "$IPDEV" -z --deadratio=5
