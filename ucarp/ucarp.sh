bb6558aabd35e6ed421217d4dde98118f9c48590
Tit Petric <tit.petric@monotek.net>
2016-12-29 10:36:28 +0100

    pass opts

--- ucarp/ucarp.sh
+++ ucarp/ucarp.sh
@@ -2,11 +2,10 @@
 set -e
 
 [ -z "$VHID" ] && VHID=`printf '%d' 0x$(echo $VIP | md5sum |cut -b1-2)`
-echo "Using id: $VHID"
 
 NET=`ip addr show dev $DEV|awk '/inet[^6]/ {print $2}'`
 RIP=`echo $NET|cut -d/ -f1`
 
-echo "REAL IP of UCARP instance: $RIP"
+echo "Startung UCARP ID $VHID IP $RIP OPTS $OPTS"
 
-exec ucarp -i "$DEV" -s "$RIP" -v "$VHID" -p "$PASS" -a "$VIP" -x "$IPS" -x "$IPDEV" -u /ucarp/up.sh -d /ucarp/down.sh -z --deadratio=5
+exec ucarp -i "$DEV" -s "$RIP" -v "$VHID" -p "$PASS" -a "$VIP" -x "$IPS" -x "$IPDEV" -u /ucarp/up.sh -d /ucarp/down.sh $OPTS -z --deadratio=5
